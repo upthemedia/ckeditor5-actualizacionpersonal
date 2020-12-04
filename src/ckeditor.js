b830e86baabfdb4b7bfae915a85c205389938e20
cristoj <cristojvt@gmail.com>
2020-12-04 13:58:17 +0100

    update with image plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -9,17 +9,17 @@ import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
 
 class Editor extends ClassicEditor {}
 
@@ -31,17 +31,17 @@ Editor.builtinPlugins = [
 	Essentials,
 	Heading,
 	Image,
+	ImageResize,
+	ImageStyle,
+	ImageToolbar,
 	ImageUpload,
-	Indent,
 	Italic,
 	Link,
 	List,
 	Paragraph,
 	PasteFromOffice,
-	SimpleUploadAdapter,
 	Table,
-	TableToolbar,
-	TextTransformation
+	TableToolbar
 ];
 
 export default Editor;
